c25c41e0cf23d3ee16f4e9de579afaf97f9d3423
Nina <n1ska@outloo.com>
2023-05-07 10:42:56 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 13674; //стоимость билета
+        System.out.println("Стоимость билета " + x);
+        System.out.println("Начислено " + (x / 20) + " милей(я)"                 );
+    }
+}
