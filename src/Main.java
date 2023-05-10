de06b66b960f199073c34e748a9b220a6bdf16a7
Nina <n1ska@outloo.com>
2023-05-10 17:06:15 +0300

    fixes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 13674; //стоимость билета
-        System.out.println("Стоимость билета " + x);
-        System.out.println("Начислено " + (x / 20) + " милей(я)"                 );
+        int price = 13674; //стоимость билета
+        System.out.println("Стоимость билета " + price);
+        System.out.println("Начислено " + (price / 20) + " милей(я)"                 );
     }
 }
